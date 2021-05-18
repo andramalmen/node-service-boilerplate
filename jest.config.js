fa5764123d009b4b5a7589c691e61093aaf8dc1c
Andra Malmen <andramalmen@gmail.com>
2021-05-18 22:59:31 +0300

    Update dependencies and config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
-    modulePathIgnorePatterns: ['<rootDir>/build/'],
+    modulePathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/config/'],
 };
